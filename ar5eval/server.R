777adee26a1fd035df7892d87c1b25b51a7c064a
leonawicz <matt.rstat@gmail.com>
2017-02-20 17:28:07 -0700

    Provide theme to module calls.

--- ar5eval/server.R
+++ ar5eval/server.R
@@ -1,7 +1,7 @@
 library(dplyr)
 library(purrr)
 library(ggplot2)
-.plottheme <- theme(panel.grid.major=element_line(size = .5, color = "grey"),
+.theme <- theme(panel.grid.major=element_line(size = .5, color = "grey"),
                     plot.title=element_text(hjust=0),
                     axis.line=element_line(size=.7, color="black"),
                     axis.ticks.length=unit(0.35,"cm"),
@@ -30,7 +30,7 @@ shinyServer(function(input, output, session) {
       aes_string(x="GCM", y="Mean_Rank", ymin="Min_Rank", ymax="Max_Rank", colour=clrby())) +
       geom_point(position=pos) + geom_crossbar(width=0.5, position=pos)
     if(input$fctby!="") g <- g + facet_wrap(as.formula(paste0("~", input$fctby)), scales="free")
-    g + .plottheme + 
+    g + .theme + 
       theme(axis.title.x=element_blank(), axis.text.x=element_blank(), axis.ticks.x=element_blank()) +
       labs(title="Spatial bootstrap GCM performance rankings", 
            subtitle=bquote(italic(.(subtitle))), 
@@ -45,7 +45,7 @@ shinyServer(function(input, output, session) {
                 aes_string(x="GCM", y="PropTop5", fill=clrby())) +
       geom_bar(stat="identity", position=pos, colour="black", width=0.5)
     if(input$fctby!="") g <- g + facet_wrap(as.formula(paste0("~", input$fctby)), scales="free")
-    g + .plottheme + theme(axis.text.x=element_text(angle=45, hjust=1, vjust=1)) +
+    g + .theme + theme(axis.text.x=element_text(angle=45, hjust=1, vjust=1)) +
       labs(title="Probability of GCM among top five performers", 
            subtitle=bquote(italic(.(subtitle))), 
            x="GCM", y="P(among top five performing GCMs)")
@@ -53,23 +53,23 @@ shinyServer(function(input, output, session) {
   
   dom <- reactive(input$tabs)
   
-  callModule(spbootMod, "sbAK", "AK", dom)
-  callModule(spbootMod, "sbAK_land", "AK_land", dom)
-  callModule(spbootMod, "sbAK_water", "AK_water", dom)
-  callModule(spbootMod, "sbCAN", "CAN", dom)
-  callModule(spbootMod, "sbAKCAN", "AKCAN", dom)
-  callModule(spbootMod, "sb6090N", "6090N", dom)
-  callModule(spbootMod, "sb2090N", "2090N", dom)
-  callModule(spbootMod, "sblow48", "low48", dom)
-  callModule(spbootMod, "sbpacif", "pacif", dom)
+  callModule(spbootMod, "sbAK", "AK", dom, .theme)
+  callModule(spbootMod, "sbAK_land", "AK_land", dom, .theme)
+  callModule(spbootMod, "sbAK_water", "AK_water", dom, .theme)
+  callModule(spbootMod, "sbCAN", "CAN", dom, .theme)
+  callModule(spbootMod, "sbAKCAN", "AKCAN", dom, .theme)
+  callModule(spbootMod, "sb6090N", "6090N", dom, .theme)
+  callModule(spbootMod, "sb2090N", "2090N", dom, .theme)
+  callModule(spbootMod, "sblow48", "low48", dom, .theme)
+  callModule(spbootMod, "sbpacif", "pacif", dom, .theme)
   
-  callModule(compositeMod, "AK", "AK", dom)
-  callModule(compositeMod, "AK_land", "AK_land", dom)
-  callModule(compositeMod, "AK_water", "AK_water", dom)
-  callModule(compositeMod, "CAN", "CAN", dom)
-  callModule(compositeMod, "AKCAN", "AKCAN", dom)
-  callModule(compositeMod, "6090N", "6090N", dom)
-  callModule(compositeMod, "2090N", "2090N", dom)
-  callModule(compositeMod, "low48", "low48", dom)
-  callModule(compositeMod, "pacif", "pacif", dom)
+  callModule(compositeMod, "AK", "AK", dom, .theme)
+  callModule(compositeMod, "AK_land", "AK_land", dom, .theme)
+  callModule(compositeMod, "AK_water", "AK_water", dom, .theme)
+  callModule(compositeMod, "CAN", "CAN", dom, .theme)
+  callModule(compositeMod, "AKCAN", "AKCAN", dom, .theme)
+  callModule(compositeMod, "6090N", "6090N", dom, .theme)
+  callModule(compositeMod, "2090N", "2090N", dom, .theme)
+  callModule(compositeMod, "low48", "low48", dom, .theme)
+  callModule(compositeMod, "pacif", "pacif", dom, .theme)
 })
