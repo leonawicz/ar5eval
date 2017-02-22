2d824ed483bf28301ee9ff12270fa1608cefec69
leonawicz <matt.rstat@gmail.com>
2017-02-22 13:26:25 -0700

    Update FAQ

--- ar5eval/faq.R
+++ ar5eval/faq.R
@@ -12,6 +12,13 @@ bsCollapse(id="faq",
   bsCollapsePanel("What is ERA-40?",
     p("Add text here, include link...", style="text-align: justify;"), 
       style="info"),
+  bsCollapsePanel("The domain map only shows a rectangle over Alaska; what are the 'land' and 'ocean' domains?",
+    p("For land- and ocean-specific domains, the rectangle domain over Alaska is the same,
+      but the GCM and ERA-40 grid cells within that domain are subset to those exclusively over land or ocean, respectively.
+      This was done to investigate the potential influence on Alaska domain model selection
+      of land vs. ocean grid cells in the analysis.",
+      style="text-align: justify;"), 
+    style="info"),
   bsCollapsePanel("Why are composite GCMs not always ordered by lowest error?",
     gcm_inclusion,
     style="info"),
