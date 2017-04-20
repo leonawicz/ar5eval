3c0aae2b9822480f5635150635cb2a328e4a6d37
leonawicz <matt.rstat@gmail.com>
2017-04-19 21:28:51 -0600

    Composite output summaries are now variable-specific and not based strictly on the normalized climate variable.

--- ar5eval/global.R
+++ ar5eval/global.R
@@ -17,16 +17,7 @@ grp_vars <- c("", "Spatial domain"="Domain", "Error statistic"="Stat", "Climate 
 gcm_order <- c("Fixed (alphabetical)"="fixed", "Reorder each data set (mean rank)"="mean")
 
 gcm_inclusion <- tagList(
-  p("The composite GCMs shown here use a fixed ensemble membership order that does not vary with climate variable.
-    Order of inclusion follows individual GCM rankings based on mean annual estimated error over the selected
-    spatial domain using the selected error statistic for the integrated climate variable.
-    This is why when viewing precipitation, sea level pressure or temperature, test results may appear
-    less optimal and the orange line representing individual GCM error in the second permutation results plot 
-    does not always increase monotonically like it does when viewing results for the integrated variable.", style="text-align: justify;"),
-  p("Results would be different for the three distinct climate variables if the order of GCM inclusion in ensembles of
-    increasing size were based on the respective climate variable-specific GCM performance rankings.
-    However, this evaluation does not aim to rank and select GCMs based on a single climate variable.
-    Instead, what is more clear is how cross-variable model selection merges GCM performance from all three
-    climate variables and that it may yield a composite model set that is notably different 
-    from those that would be chosen using a single climate variable.", style="text-align: justify;")
+  p("The order in which GCMs are including in composite models of increasing of size uses individual GCM rankings.
+    Rank is based on mean annual estimated error for the selected climate variable over the selected
+    spatial domain using the selected error statistic.", style="text-align: justify;")
 )
