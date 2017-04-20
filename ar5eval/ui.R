7665471481733fd5c2f264ba1867bde7a10a5f48
leonawicz <matt.rstat@gmail.com>
2017-04-20 11:49:23 -0600

    Fix file name

--- ar5eval/ui.R
+++ ar5eval/ui.R
@@ -7,7 +7,7 @@ dashboardPage(
             tags$a(href="http://snap.uaf.edu", target="_blank",
                    tags$img(src="SNAP_acronym_100px.png", width="100%", alt="SNAP"), style="padding: 10px; margin: 0px;")
     ),
-    tags$head(includeScript("ga-standage.js"), includeScript("ga-allapps.js"))
+    tags$head(includeScript("ga-ar5eval.js"), includeScript("ga-allapps.js"))
   ),
   dashboardSidebar(
     #useToastr(),
