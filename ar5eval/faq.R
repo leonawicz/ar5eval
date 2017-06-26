6d2e37d2b65134152ef7dd19a562714951494ad7
leonawicz <matt.rstat@gmail.com>
2017-06-26 09:43:38 -0600

    Update FAQ

--- ar5eval/faq.R
+++ ar5eval/faq.R
@@ -1,11 +1,9 @@
 bsCollapse(id="faq",
   bsCollapsePanel("What is a GCM?",
-    p("General Circulation Models (GMCs) are used to depict how climate processes respond to the composition of various gases in the atmosphere.
+    p("General Circulation Models (GCMs) are used to depict how climate processes respond to the composition of various gases in the atmosphere.
       Using future projections of the composition of gases in the atmosphere, projections of future climate can be made.
-      For this work, the GCMs provide different projections of future climate that are used to inform projections of future fire activity.
-      If you are interested in exploring the range of burned area with these GCMs,
-      the GFDL-CM3 and NCAR-CCSM4 models tend to correspond to the projections with the most area burned
-      and the MRI-CGCM3 tends to have the least area burned. For more information, please see the following link.", style="text-align: justify;"),
+      The climate models used in this comparative model evaluation are based on the 
+      Coupled Model Intercomparison Project Phase 5 (CMIP5) / fifth assessment report (AR5).", style="text-align: justify;"),
     a("Intergovernmental Panel on Climate Change GCM guide",
       href="http://www.ipcc-data.org/guidelines/pages/gcm_guide.html", target="_blank"),
     style="info"),
