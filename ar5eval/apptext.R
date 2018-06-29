0d13c8a0bda1326a12de1f3ba39224fb357c2775
leonawicz <matt.rstat@gmail.com>
2018-06-29 08:01:37 -0600

    Update citation.

--- ar5eval/apptext.R
+++ ar5eval/apptext.R
@@ -6,7 +6,7 @@ app_intro_message <- "<h4>Explore SNAP's AR5/CMIP5 climate model evaluation.</h4
 GCM performance is ranked based on minimum error with respect to a European Re-Analysis (ERA-40) baseline data set using several error metrics.</p>
 <p style='text-align:justify;'>A detailed tour is available in the sidebar. Additional information can be found on the <strong>Information</strong> tab. Click to dismiss this message at any time.</p>
 <p style='text-align:justify;'>This is an application supplement to the publication:</p>
-<p style='text-align:justify;'>Walsh, J. E., Bhatt, U. S., Littell, J. S., Leonawicz et al. 2018. Downscaling of climate model output for Alaskan stakeholders. Environmental Modelling & Software.</p>
+<p style='text-align:justify;padding-left:20px;padding-right:20px;'>Walsh, J. E., Bhatt, U. S., Littell, J. S., Leonawicz et al. 2018. Downscaling of climate model output for Alaskan stakeholders. Environmental Modelling & Software.</p>
 <p style='text-align:justify;'>See the publication for additional context and details.</p>"
 
 about_app <- tagList(
